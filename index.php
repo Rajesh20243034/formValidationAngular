b233660ebd846b639e2fc386807660e66d4c5c0b
Rajesh20243034 <rajesh.patelp3034@gmail.com>
2015-03-04 16:31:39 +0530

    basic file structured has prepared

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Angular Conditional Classes</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<style type="text/css">
+		
+	</style>
+</head>
+<body>
+	<div ng-app>
+		<form>
+			<label for="username">User</label>
+			<br /><label for="email">Email</label>
+			<br /><label for="password">Password</label>
+		</form>
+	</div>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script  src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.min.js"></script>
+</body>
+</html>
